97677dd24db1d5c3bd1413278e0853c62a5a9d01
Pedro Cout <pedro.coutinho@estudante.ufjf.br>
2021-03-20 14:52:44 -0300

    Adiciona remoção de Sprites na Cena

--- js/Cena.js
+++ js/Cena.js
@@ -21,34 +21,35 @@ export default class Cena {
     adicionar(sprite) {
         this.sprites.push(sprite);
     }
-    passo(dt){
+    passo(dt) {
         for (const sprite of this.sprites) {
             sprite.passo(dt);
         }
     }
 
-    quadro(t){
+    quadro(t) {
         this.t0 = this.t0 ?? t;
         this.dt = (t - this.t0)/1000;
 
         this.passo(this.dt);
         this.desenhar();
         this.checaColisao();
+        this.removerSprites();
 
         this.iniciar();
         this.t0 = t;
     }
-    iniciar(){
+    iniciar() {
         this.idAnim = requestAnimationFrame((t) => {
             this.quadro(t);
         });
     }
-    parar(){
+    parar() {
         cancelAnimationFrame(this.idAnim);
         this.t0 = null;
         this.dt = 0;
     }
-    checaColisao(){
+    checaColisao() {
         for (let a = 0; a < this.sprites.length - 1; a++) {
             const spriteA = this.sprites[a];
             for (let b = a+1; b < this.sprites.length; b++) {
@@ -59,12 +60,20 @@ export default class Cena {
             }
         }
     }
-    quandoColidir(a, b){
-        if(!this.aRemover.includes(a)){
+    quandoColidir(a, b) {
+        if(!this.aRemover.includes(a)) {
             this.aRemover.push(a);
         }
-        if(!this.aRemover.includes(b)){
+        if(!this.aRemover.includes(b)) {
             this.aRemover.push(b);
+        }
+    }
+    removerSprites() {
+        for (const alvo of this.aRemover) {
+            const idx = this.sprites.indexOf(alvo);
+            if (idx >= 0) {
+                this.sprites.splice(idx, 1);
+            }
         }
     }
 }

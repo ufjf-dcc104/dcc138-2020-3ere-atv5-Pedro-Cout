d5dd911c074bed4e5ce83852686d6a29230014b3
Pedro Cout <pedro.coutinho@estudante.ufjf.br>
2021-03-20 21:46:46 -0300

    Adiciona um método acabou()

--- js/AssetManager.js
+++ js/AssetManager.js
@@ -1,4 +1,4 @@
-export default class AssetManager{
+export default class AssetManager {
     constructor() {
         this.aCarregar = 0;
         this.carregadas = 0;
@@ -7,18 +7,28 @@ export default class AssetManager{
 
     carregaImagem(chave, source) {
         const img1= new Image();
+        img1.addEventListener("load",  () => {
+            console.log(`Imagem ${this.carregadas}/${this.aCarregar} carregada!`);
+            this.carregadas++;
+        });
+
         img1.src = source;
         this.imagens.set(chave, img1);
+        this.aCarregar++;
     }
 
     img(chave) {
         return this.imagens.get(chave);
     }
-    
+
     progresso() {
         if(this.aCarregar > 0) {
             return `${(this.carregadas/this.aCarregar * 100).toFixed((2))}%`;
         }
         return "Nada a carregar";
+    }
+
+    acabou() {
+        return this.carregadas === this.aCarregar;
     }
 }

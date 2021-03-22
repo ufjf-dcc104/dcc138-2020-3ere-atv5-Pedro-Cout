f15e020cdf93659941e24d4a006d51b1c51d9bdd
Pedro Cout <pedro.coutinho@estudante.ufjf.br>
2021-03-22 01:25:24 -0300

    Refatora Sprite para o centro de massa

--- js/Sprite.js
+++ js/Sprite.js
@@ -2,7 +2,15 @@ export default class Sprite {
     /*
     É responsável por modelar algo se move na tela.
     */
-    constructor({x=100, y=100, w=20, h=20, color="white", vx = 0, vy = 0}={}){
+    constructor({
+        x=100,
+        y=100,
+        w=20,
+        h=20,
+        color="white",
+        vx = 0,
+        vy = 0,
+    } = {}) {
         this.x = x;
         this.y = y;
         this.vx = vx;
@@ -13,7 +21,7 @@ export default class Sprite {
     }
     desenhar(ctx){
         ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.w, this.h);
+        ctx.fillRect(this.x - this.w/2, this.y - this.h/2, this.w, this.h);
     }
     passo(dt){
         this.x = this.x + this.vx*dt;
@@ -21,10 +29,10 @@ export default class Sprite {
     }
     colidiuCom(outro){
         return !(
-            this.x > outro.x + outro.w ||
-            this.x + this.w < outro.x ||
-            this.y > outro.y + outro.h ||
-            this.y + this.h < outro.y
+            this.x - this.w/2 > outro.x + outro.w/2 ||
+            this.x + this.w/2 < outro.x - outro.w/2 ||
+            this.y - this.h/2 > outro.y + outro.h/2 ||
+            this.y + this.h/2 < outro.y - outro.h/2
         );
     }
 }

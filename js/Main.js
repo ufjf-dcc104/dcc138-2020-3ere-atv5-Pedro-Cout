adfdd4cbecc102412f98e8d8e4aa904b3a692b84
Pedro Cout <pedro.coutinho@estudante.ufjf.br>
2021-03-28 02:11:45 -0300

    Detecta erros linhas 41, 42, 46 e 47 da Main.js

--- js/Main.js
+++ js/Main.js
@@ -5,16 +5,23 @@ import Game from "./Game.js";
 import CenaJogo from "./CenaJogo.js";
 import CenaCarregando from "./CenaCarregando.js";
 import CenaFim from "./CenaFim.js";
+import Sprite from "./Sprite.js";
+import Mapa from "./Mapas.js";
+import modeloMapa1 from "../maps/mapa1.js";
+
 
 const input = new InputManager();
 const mixer = new Mixer(10);
 const assets = new AssetManager(mixer);
+
 
 assets.carregaImagem("garota", "assets/garota.png");
 assets.carregaImagem("esqueleto", "assets/skelly.png");
 assets.carregaImagem("orc", "assets/orc.png");
 assets.carregaAudio("moeda", "assets/coin.wav");
 assets.carregaAudio("explosao", "assets/boom.wav");
+
+
 
 const canvas = document.querySelector("canvas");
 canvas.width = 14 * 32;
@@ -28,14 +35,17 @@ input.configurarTeclado({
     " ": "PROXIMA_CENA",
  });
 
-const game = new Game(canvas, assets, input);
 
+const game = new Game(canvas, assets, input);
 const cena0 = new CenaCarregando();
-const cena1 = new CenaJogo();
-const cena2 = new CenaFim();
+//const cena1 = new Fase_Map1();
+//const cena2 = new Fase_Map2();
+const cena3 = new CenaFim();
+
 game.adicionarCena("carregando", cena0);
-game.adicionarCena("jogo", cena1);
-game.adicionarCena("fim", cena2);
+//game.adicionarCena("fase1", cena1);
+//game.adicionarCena("fase2", cena2);
+game.adicionarCena("fim", cena3);
 game.iniciar();
 
 document.addEventListener("keydown", (e) => {
